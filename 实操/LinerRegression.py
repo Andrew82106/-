58125c5d4287f5971bb507c4a40d97d018e1980f
Andrew82106 <qq281512383@163.com>
2023-02-03 20:31:35 +0800

    II commit. learning how to code Liner Regression today

--- 实操/LinerRegression.py
+++ 实操/LinerRegression.py
@@ -1,26 +1,40 @@
 import torch
 from torch.utils.data import DataLoader, TensorDataset
+from torch import nn, optim
 
 
-def MakeData() -> DataLoader:
+def MakeData():  # 造数据函数！
     """
     X:[a, a, a]
     Y:[b]
     Y=Xw+b+eps
     :return:
     """
-    X = torch.normal(0, 1, (200, 3))
-    w = torch.tensor([1, 2, 3], dtype=torch.float)
+    X = torch.normal(0, 1, (200, 4))
+    w = torch.tensor([1, 2, 3, 100], dtype=torch.float)
     b = -12.1
     Y = torch.matmul(X, w) + b
     Y += torch.normal(0, 0.0001, Y.shape)
     res = TensorDataset(X, Y)
-    return DataLoader(dataset=res, batch_size=3, shuffle=1)
+    return DataLoader(dataset=res, batch_size=10, shuffle=1), X, Y
+
+
+def Train(Data_iterator: DataLoader, X, Y):
+    model = nn.Sequential(nn.Linear(4, 1))
+    loss = nn.MSELoss()
+    trainer = optim.SGD(model.parameters(), lr=0.1)
+    epoch = 1
+    while epoch < 100:
+        for x, y in Data_iterator:
+            l = loss(model(x).reshape(-1, 1), y.reshape(-1, 1))
+            trainer.zero_grad()
+            l.backward()
+            trainer.step()
+
+        print(f'epoch:{epoch} loss:{loss(model(X).reshape(-1, 1), Y.reshape(-1, 1))}')
+        epoch += 1
+    # 训练倒是大概写出来了，但是模型参数不知道是个啥情况
 
 
 if __name__ == '__main__':
-    Data_iterator = MakeData()  # 造数据函数！
-    for x, y in Data_iterator:
-        print(x[0])
-        print(y[0])
-        break
+    Train(*MakeData())
