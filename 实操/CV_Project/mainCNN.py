ce631359addd9d30ee218f73248abe7eb5b2d3a5
Andrew82106 <qq281512383@163.com>
2024-02-20 12:48:25 +0800

    update

--- 实操/CV_Project/mainCNN.py
+++ 实操/CV_Project/mainCNN.py
@@ -4,7 +4,7 @@ import torch
 from baseConfig import *
 from CNN.LoadData import *
 from CNN.model import *
-from torch.optim import Adam
+from torch.optim import Adam, SGD
 from torch.nn import CrossEntropyLoss, MSELoss
 import tqdm
 from CNN.OptimLion import Lion
@@ -37,6 +37,7 @@ if __name__ == '__main__':
     model = CNN()
     model.to(device)
     # optimer = Adam(model.parameters(), lr=learning_rate)
+    # optimer = SGD(model.parameters(), lr=learning_rate)
     optimer = Lion(model.parameters(), lr=learning_rate)
     loss = CrossEntropyLoss()
     loss1 = MSELoss()
@@ -58,6 +59,7 @@ if __name__ == '__main__':
     bestModel = None
     acc_history = []
     loss_history = []
+    vali_loss_history = []
     for E in range(epoch):
 
         sumLoss = 0
@@ -68,32 +70,42 @@ if __name__ == '__main__':
             b = b.to(device)
             res = model(a)
             batchLoss = loss(res, b)
+            cnt += 1
             optimer.zero_grad()
             batchLoss.backward()
             optimer.step()
             sumLoss += batchLoss
-        print(f'loss={float(sumLoss)}')
-        loss_history.append(float(sumLoss))
+        print(f'loss={float(sumLoss)/cnt}')
+        loss_history.append(float(sumLoss)/cnt)
 
         model.eval()
         sumCount = 0
         allCount = 0
+        sumLoss = 0
+        cnt = 0
         with torch.no_grad():
             for img, label in TestLoader:
                 img = img.to(device).type(torch.float32)
                 label = label.to(device)
-                predicted = torch.argmax(model(img), dim=1)
+                P = model(img)
+                predicted = torch.argmax(P, dim=1)
+                batchLoss = loss(P, label)
+                sumLoss += batchLoss
+                cnt += 1
                 equal_elements = torch.eq(predicted, label)
                 count = torch.sum(equal_elements).item()
                 sumCount += count
                 allCount += len(predicted)
             print(f"Test acc:{sumCount / allCount}")
+            print(f"Test loss:{float(sumLoss) / cnt}")
             if sumCount / allCount > acc:
                 acc = sumCount / allCount
                 bestModel = model
                 print("got best performance")
             acc_history.append(sumCount / allCount)
+            vali_loss_history.append(float(sumLoss) / cnt)
 
     saveModel(bestModel, os.path.join(CNNPath, 'modelCache', f"CNN_best_acc={acc}.pth"))
     draw_line_chart(acc_history, X='epoch', Y='accuracy', title='accuracy history', saveLocation=os.path.join(CNNPath, 'trainingLogs', 'accuracy.png'))
     draw_line_chart(loss_history, X='epoch', Y='loss', title='loss history', saveLocation=os.path.join(CNNPath, 'trainingLogs', 'loss.png'))
+    draw_line_chart(vali_loss_history, X='epoch', Y='loss', title='validation loss history', saveLocation=os.path.join(CNNPath, 'trainingLogs', 'vali_loss.png'))
